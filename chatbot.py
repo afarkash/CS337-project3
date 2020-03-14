e82ac71ff5613a995e7c0b2779d58e5973eda099
nathanlxu <nathanlxu@gmail.com>
2020-03-14 02:47:08 -0500

    chatbot now takes a recipe and stores ingredients and directions as instance variables, and also inherits RecipeTransformer

--- chatbot.py
+++ chatbot.py
@@ -1,38 +1,41 @@
-import transform_recipe
-import scraper.py
+from transform_recipe import RecipeTransformer
+import scraper
 
-class Chatbot:
-    def __init__(self):
-        transformer = RecipeTransformer()
-        rf = RecipeFetcher()
-        url = ''
-        title = ''
-        ingredients = []
-        directions = []
-        got_recipe = True
-        directions_dict = {}
+class Chatbot(RecipeTransformer):
+    def __init__(self, recipe):
+        super().__init__()
+        # self.transformer = RecipeTransformer()
+        # self.rf = scraper.RecipeFetcher()
+        # self.url = ''
+        # self.title = ''
+        self.ingredients = self.original_recipe(recipe)['ingredients']
+        self.directions = self.original_recipe(recipe)['directions']
+        # self.got_recipe = True
+        # self.directions_dict = {}
 
 
-    def get_url(self, url):
-        self.url = url
+    # def get_url(self, url):
+    #     self.url = url
+    #
+    # def get_recipe(self):
+    #     recipe_dict = self.rf.scrape_recipe(self.url)
+    #     self.title = recipe_dict["title"]
+    #     self.ingredients = recipe_dict["ingredients"]
+    #     self.directions = recipe_dict["directions"]
+    #
+    # def print_ingredients(self):
+    #     if self.got_recipe:
+    #         for ing in self.ingredients:
+    #             print(ing)
+    #
+    # def print_directions(self):
+    #     if self.got_recipe:
+    #         for i in range(len(self.ingredients) - 1):
+    #             self.directions_dict[i] = self.directions[i]
+    #             print(str(i) + ". " + self.directions[i])
 
-    def get_recipe(self):
-        recipe_dict = rf.scrape_recipe(self.url)
-        self.title = recipe_dict["title"]
-        self.ingredients = recipe_dict["ingredients"]
-        self.directions = recipe_dict["directions"]
-
-    def print_ingredients(self):
-        if(got_recipe):
-            for ing in self.ingredients:
-                print(ing)
-
-    def print_directions(self):
-        if(got_recipe):
-            for i in range(len(self.ingredients) - 1):
-                self.directions_dict[i] = self.directions[i]
-                print(str(i) + ". " + self.directions[i])
-    
+cb = Chatbot('meat lasagna')
+print(cb.ingredients)
 
 
         
